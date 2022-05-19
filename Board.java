da0f7168147b979183f71f65cda75dccc66d1a83
oliver <oliver@oliver-Bravo-15-B5DD>
2022-05-19 13:01:26 +1200

    Trying to troubleshoot the winning check method.

--- Board.java
+++ Board.java
@@ -56,11 +56,16 @@ public class Board {
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
 	public boolean hasWon(Player thePlayer, int playerRow, int playerCol) {
-		 // check if player has 3-in-that-row
+		System.out.println(cells[2][playerCol].content);
+		System.out.println(cells[playerRow][0].content);
+		
+		
+		// check if player has 3-in-that-row
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		//Checks if player has one by row.
-		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer )
+		if(cells[2][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[0][playerCol].content == thePlayer )
+			
 			return true;
 			
 			
